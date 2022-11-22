c90a8658f22482c2f943181f3454f2c509665f8f
Nastassia Hramadskaya <anastasiyagromadskaya@gmail.com>
2022-11-22 20:30:38 +0300

    Add Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 8;
-int numberB = 90;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
