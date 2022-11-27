56519d3d25058895f096db582eb857f3b54b5d05
Nastassia Hramadskaya <anastasiyagromadskaya@gmail.com>
2022-11-27 17:00:49 +0300

    add task 1 seminar 2

---
+++ Sem2_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая 
+// выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Clear();
+int randomDigit = new Random().Next(10,100);
+Console.WriteLine($"Случайное число: {randomDigit}");
+
+int ostatok = randomDigit%10;
+Console.WriteLine($"Последняя цифра: {ostatok}");
+
+int firstDigit = randomDigit/10;
+Console.WriteLine($"Первая цифра: {firstDigit}");
+
+if (ostatok > firstDigit)
+{
+    Console.WriteLine($"Наибольшая цифра: {ostatok}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра: {firstDigit}");
+}
