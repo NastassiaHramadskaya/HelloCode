f01487c1fcf6a91d4199d176a4777a5994e0fc90
Nastassia Hramadskaya <anastasiyagromadskaya@gmail.com>
2022-11-27 17:05:03 +0300

    add task 1 seminar 2 additional solution

--- Sem2_Task1/Program.cs
+++ Sem2_Task1/Program.cs
@@ -2,10 +2,10 @@
 // выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
 
 Console.Clear();
-int randomDigit = new Random().Next(10,100);
+int randomDigit = new Random().Next(10,100); //случайное число от 10 до 99 включительно
 Console.WriteLine($"Случайное число: {randomDigit}");
 
-int ostatok = randomDigit%10;
+int ostatok = randomDigit%10; //остаток от деления на 10
 Console.WriteLine($"Последняя цифра: {ostatok}");
 
 int firstDigit = randomDigit/10;
@@ -19,3 +19,10 @@ else
 {
     Console.WriteLine($"Наибольшая цифра: {firstDigit}");
 }
+
+// int max = firstDigit;
+// if (ostatok > firstDigit)
+//     max = ostatok;
+// Console.WriteLine($"Наибольшая цифра: {max}");
+
+
